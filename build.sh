8a2ccd251c882de89f3ca6b176f4ff8c53e8810e
Egidijus <egidijus@anotherlan.com>
2015-12-17 18:00:22 +0000

    [cleanup] even more consistent

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t "alpine-builder" .
-docker run --rm -it -e RSA_PRIVATE_KEY="$(cat ./your-rsa-key.rsa)" -v $(pwd)/your-rsa-key.rsa.pub:/etc/apk/keys/abuild.rsa.pub -v $(pwd):/home/builder/package -v $(pwd)/packages:/home/builder/packages "alpine-builder"
+docker build -t "alpine-jazz-hands" .
+docker run --rm -it -e RSA_PRIVATE_KEY="$(cat ./your-rsa-key.rsa)" -v $(pwd)/your-rsa-key.rsa.pub:/etc/apk/keys/abuild.rsa.pub -v $(pwd):/home/builder/package -v $(pwd)/packages:/home/builder/packages "alpine-jazz-hands"
